96c4ab979ab091904565a2a226f440b4b6b3c28f
Olaf Conradi <olaf@conradi.org>
2020-07-08 11:01:38 +0200

    Allow failures when parsing dates

--- utils.py
+++ utils.py
@@ -6,17 +6,25 @@ _logger = logging.getLogger(__name__)
 
 
 def parse_and_format_date(datestr, **kwargs):
-    dt = dateparser.parse(datestr, **kwargs)
-    fmtdate = dt.strftime("%Y-%m-%d")
-    _logger.info("Parsing %s result %s", datestr, fmtdate)
-    return fmtdate
+    try:
+        dt = dateparser.parse(datestr, **kwargs)
+        fmtdate = dt.strftime("%Y-%m-%d")
+        _logger.info("Parsing %s result %s", datestr, fmtdate)
+        return fmtdate
+    except:
+        _logger.exception("Could not parse %s as date", datestr)
+        return ""
 
 
 def parse_and_format_month_year(datestr, **kwargs):
-    dt = dateparser.parse(datestr, **kwargs)
-    fmtdate = dt.strftime("%Y-%m")
-    _logger.info("Parsing %s result %s", datestr, fmtdate)
-    return fmtdate
+    try:
+        dt = dateparser.parse(datestr, **kwargs)
+        fmtdate = dt.strftime("%Y-%m")
+        _logger.info("Parsing %s result %s", datestr, fmtdate)
+        return fmtdate
+    except:
+        _logger.exception("Could not parse %s as date", datestr)
+        return ""
 
 
 def select_highest_amount(matches):
